c873b64fa58b1f91dbda4afae6b3c4a5db3ad552
Unknown <jonlopezgarrido@gmail.com>
2017-05-02 21:51:35 +0200

    plataforma y bola (jugador)

---
+++ baseball/Assets/scripts/controlJugador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlJugador : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();		
+	}
+	
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		transform.Rotate (new Vector3 (moveVertical, 0.0f, moveHorizontal) * Time.deltaTime * 100);
+
+		//rb.AddForce (movement * speed);
+
+	}
+}
